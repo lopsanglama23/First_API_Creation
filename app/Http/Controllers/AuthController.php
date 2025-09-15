aaec07cb248bf20fcbee56d9a296d52f42e11653
lopsang <lopsang900@gmail.com>
2025-09-15 14:25:51 +0545

    solving logout error

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\LogoutRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
