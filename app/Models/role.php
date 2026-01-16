1b9b7a82365d881e90576e65cdee60fc0e568926
lopsang <lopsang900@gmail.com>
2026-01-16 13:45:29 +0545

    added new things

---
+++ app/Models/role.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class role extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    protected $casts = [
+        'name' =>'string'
+    ];
+    protected $hidden = [
+        'id'
+    ];
+}
